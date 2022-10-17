a3c74c0d36d8cd59c8420abaa67ae7a532fde4d7
ivivix90 <wiszenkoigor@gmail.com>
2022-10-17 14:47:49 +0200

    security config made

--- src/main/java/com/example/userservice/UserserviceApplication.java
+++ src/main/java/com/example/userservice/UserserviceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
 
@@ -13,6 +15,11 @@ import java.util.ArrayList;
 public class UserserviceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 	@Bean
--- src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -6,16 +6,37 @@ import com.example.userservice.repo.RoleRepo;
 import com.example.userservice.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements com.example.userservice.Service.UserService {
+public class UserServiceImpl implements com.example.userservice.Service.UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if(user==null){
+            log.error("User not found in DB");
+            throw new UsernameNotFoundException("User not found in DB");
+        } else {
+            log.info("User found in DB: {} ", username);
+        }
+        Collection< SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
 
     @Override
     public User saveUser(User user) {
@@ -49,4 +70,6 @@ public class UserServiceImpl implements com.example.userservice.Service.UserServ
         log.info("Fetching all users: ");
         return userRepo.findAll();
     }
+
+
 }
